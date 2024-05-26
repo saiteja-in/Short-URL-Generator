20de4c64bcd62cc217753d64e5e04ddd232f4ed0
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-26 17:39:21 +0530

    post and get operation done

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const express=require('express');
-const app=express();
-const port=8000;
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const app = express();
+const URL = require("./models/url");
+const urlRoute = require("./routes/url");
 
-app.listen(port,()=>console.log("port is connected"))
+const port = 8000;
+app.use(express.json());
+
+connectToMongoDB(
+  "mongodb+srv://saiteja:abcde@cluster0.u9jhqne.mongodb.net/short-url"
+).then(() => console.log("mongodb connected"));
+app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry=await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL)
+});
+
+app.listen(port, () => console.log("port is connected"));
---
+++ models/url.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  {
+    timestamps: true,
+  }
+);
+const URL=mongoose.model('url',urlSchema); // here 'url is just the model name 
+module.exports=URL;
+
+
+// The urls collection in your short-url database is likely created by Mongoose when you define and use the URL model in your application. Mongoose automatically creates a collection in MongoDB based on the model name, and it pluralizes the name by default.
