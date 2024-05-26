b04a5102311a13fb119a86c0439da8d9a49aa75d
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-26 21:54:27 +0530

    update

--- controllers/url.js
+++ controllers/url.js
@@ -1,16 +1,27 @@
-const URL = require('../models/url')
-const shortid=require('shortid')
-async function handleGenerateNewShortURL(req,res){
-    const body=req.body;
-    if(!body.url) return res.status(400).json({error:'url is required'})
-    const shortID=shortid();
-    await URL.create({
-        shortId:shortID,
-        redirectURL:body.url,
-        visitHistory:[]
-    })
-    return res.json({id:shortID})
+const URL = require("../models/url");
+const shortid = require("shortid");
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = shortid();
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortID });
 }
-module.exports={
-    handleGenerateNewShortURL
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({
+    shortId,
+  });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetAnalytics
+};
